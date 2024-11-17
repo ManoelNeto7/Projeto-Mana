af203a246d753d4466f27f03e36727393e976961
ManoelNeto7 <manoel.neto250297@gmail.com>
2024-11-16 21:39:43 -0300

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bem-vindo ao Painel!</h1>
+      <p>Funções adicionais podem ser implementadas aqui.</p>
+    </div>
+  );
+}
